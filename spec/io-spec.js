2f2f4548085a18df1b9d15966ef1275104b6e26b
Christopher Lane Hinson <lane@webperformance.com>
2016-04-27 23:37:36 -0400

    Initial commit

---
+++ spec/io-spec.js
@@ -0,0 +1,151 @@
+
+var Io = require('../src/index');
+
+describe('The Io object', function() {
+  it('Can return static values', function(done) {
+    Io.of(5).run().then(function(result) {
+      expect(result).toBe(5);
+      done();
+    });
+  });
+
+  it('can be applied according to the fantasyland applicative specification', function(done) {
+    //TODO read this specification carefully.
+    Io.of(function(x) { return x*2; }).ap(Io.of(5)).run().then(function(result) {
+      expect(result).toBe(10);
+      done();
+    });
+  });
+
+  it('can be chained according to the fantasyland monad specification', function(done) {
+    //TODO read this specification carefully.
+    Io.of(5).chain(function(x) {
+      return Io.of(x*2);
+    }).run().then(function(result) {
+      expect(result).toBe(10);
+      done();
+    });
+  });
+
+  it('can be chained by passing in a raw Io', function(done) {
+    Io.of(5).chain(Io.of('ten')).run().then(function(result) {
+      expect(result).toBe('ten');
+      done();
+    });
+  });
+
+  it('can be chained by a function that returns a value', function(done) {
+    Io.of(5).chain(function(five) {
+      return five+five;
+    }).run().then(function(result) {
+      expect(result).toBe(10);
+      done();
+    });
+  });
+
+  it('can be chained by a function that returns a promise', function(done) {
+    Io.get().chain(function(five) {
+      return Promise.resolve(five+five).then(function(result) { return result*2; });
+    }).run(5).then(function(result) {
+      expect(result).toBe(20);
+      done();
+    });
+  });
+
+  it('can get an embedded state', function(done) {
+    Io.get().chain(function(x) { return x+2; }).run(5).then(function(result) {
+      expect(result).toBe(7);
+      done();
+    });
+  });
+
+  it('can get an embedded state even when nested', function(done) {
+    function foo(x) {
+      return Io.get().chain(function(y) { return Promise.resolve(x+y); });
+    }
+
+    foo(5).chain(foo(6)).run(2).then(function(result) {
+      expect(result).toBe(8);
+      done();
+    });
+  });
+
+  it('can get an embedded state in fluent style', function(done) {
+    var side_effect = false;
+
+    Io.of()
+      .chain(function() { side_effect = true; })
+      .get().chain(function(x) { return Promise.resolve(x*2); })
+      .run(7).then(function(result) {
+        expect(side_effect).toBe(true);
+        expect(result).toBe(14);
+        done();
+      }, function(err) {
+        done.fail(err);
+      });
+  });
+
+  it('can insert a working value, using of(), even in fluent style', function(done) {
+    var side_effect = false;
+
+    Io.of().chain(function() { side_effect = true; })
+      .of(5).chain(function(x) {
+        expect(side_effect).toBe(true);
+        expect(x).toBe(5);
+        done();
+      }).run().catch(function(err) {
+        done.fail(err);
+      });
+  });
+
+  it('can be executed multiple times with different outcomes', function(done) {
+    var io = Io.get().chain(function(x) {
+      return x.toUpperCase();
+    });
+
+    var results = [io.run('foo'), io.run('bar'), io.run('baz'), io.run('quux')];
+
+    Promise.all(results).then(function(values) {
+      expect(values).toEqual(['FOO','BAR','BAZ','QUUX']);
+      done();
+    });
+  });
+
+  it('supports local modifications to the embedded state', function(done) {
+    var action = Io.get().chain(function(x) { return x; });
+
+    Io.local(function(x) { return x*2; }, action).run(3).then(function(result) {
+      expect(result).toEqual(6);
+      done();
+    }, function(err) {
+      done.fail(err);
+    });
+  });
+
+  it('supports local modifications to the embedded state, even in fluent style', function(done) {
+    var side_effect = false;
+
+    Io.of()
+      .chain(function() { side_effect = true; })
+      .local(function(x) { return x+1; }, Io.get())
+      .run(0)
+      .then(function(result) {
+        expect(result).toEqual(1);
+        expect(side_effect).toBe(true);
+        done();
+      }, function(err) {
+        done.fail(err);
+      });
+  });
+
+  it('can be unwrapped to get a function that takes the embedded state and returns a promise', function(done) {
+    var action = Io.of(5).chain(function(x) { return Io.get().chain(function(y) { return x+y; }); });
+
+    action.unwrap()(9).then(function(result) {
+      expect(result).toBe(14);
+      done();
+    }, function(err) {
+      done.fail(err);
+    });
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,80 @@
+
+var Io = {
+  _action : function() { throw Error('Io undefined action.'); }
+};
+
+function isIo(value) {
+  return typeof value === 'object' && Object.getPrototypeOf(value) === Io;
+}
+
+Io.of = function(value) {
+  if( isIo(this) )
+    return this.chain(Io.of(value));
+
+  return Object.assign(Object.create(Io), {
+    _action : function() {
+      return Promise.resolve(value);
+    }
+  });
+};
+
+Io.get = function() {
+  if( isIo(this) )
+    return this.chain(Io.get());
+
+  return Object.assign(Object.create(Io), {
+    _action : function(state) {
+      return Promise.resolve(state);
+    }
+  });
+};
+
+Io.map = function(f) {
+  return this.chain(function(v) {
+    return Promise.resolve(f(v));
+  });
+};
+
+Io.ap = function(v) {
+  return this.chain(function(f) { return v.map(f); });
+};
+
+Io.chain = function(f) {
+  var previous_io = this;
+
+  return Object.assign(Object.create(Io), {
+    _action : function(state) {
+      return previous_io.run(state).then(function(result) {
+        result = typeof f === 'function' ? f.call(state,result) : f;
+        if( isIo(result) )
+          result = result.run(state);
+        return Promise.resolve(result);
+      });
+    }
+  });
+};
+
+Io.local = function(modification, action) {
+  if( isIo(this) )
+    return this.chain(Io.local(modification, action));
+
+  return Io.get().chain(function(state) {
+    var local_state = modification(state);
+
+    return Io.of().chain(action).run(local_state);
+  });
+};
+
+Io.unwrap = function() {
+  return this.run.bind(this);
+};
+
+Io.run = function(state) {
+  return this._action.call(undefined, state);
+};
+
+module.exports.of = Io.of;
+module.exports.get = Io.get;
+module.exports.local = Io.local;
+module.exports.isIo = isIo;
+
